7135917717d9d9021a57187080dc769d6f356389
scoltelli27 <92338065+scoltelli27@users.noreply.github.com>
2022-09-08 17:31:47 -0300

    Changed fetch location
    Now fetch works in the App component, that way I can use that data on any child components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import "./App.css";
 import Header from "./components/Header";
 import Random from "./components/Random";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [randomMovie, setRandomMovie] = useState();
+  const [movieData, setMovieData] = useState({
+    title: "",
+    overview: "",
+    poster: "",
+  });
+
+  //Get random number
+  function randomNumber() {
+    setRandomMovie(Math.floor(Math.random() * 803033));
+  }
+
+  // useEffect to fetch the API data and store it in state
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${randomMovie}?api_key=53bdb2a3faf53552f09348f942d242c9`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (
+          data.adult === false &&
+          data.original_language === "en" &&
+          data.poster_path
+        ) {
+          console.log(data);
+          setMovieData({
+            title: data.title,
+            overview: data.overview,
+            poster: `https://image.tmdb.org/t/p/w500${data.poster_path}`,
+          });
+        } else {
+          randomNumber();
+        }
+      });
+  }, [randomMovie]);
+
   return (
     <div className="App">
       <Header />
-      <Random />
+      <Random movieData={movieData} randomNumber={randomNumber} />
     </div>
   );
 }
--- src/components/Random.js
+++ src/components/Random.js
@@ -1,52 +1,17 @@
 import React, { useState, useEffect } from "react";
+import App from "../App";
 
-export default function Random() {
-  const [randomMovie, setRandomMovie] = useState();
-  const [movieData, setMovieData] = useState({
-    title: "",
-    overview: "",
-    poster: "",
-  });
-
-  //Get random number
-  function randomNumber() {
-    setRandomMovie(Math.floor(Math.random() * 803033));
-  }
-
-  // useEffect to fetch the API data and store it in state
-  useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/${randomMovie}?api_key=53bdb2a3faf53552f09348f942d242c9`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        if (
-          data.adult === false &&
-          data.original_language === "en" &&
-          data.poster_path
-        ) {
-          console.log(data);
-          setMovieData({
-            title: data.title,
-            overview: data.overview,
-            poster: `https://image.tmdb.org/t/p/w500${data.poster_path}`,
-          });
-        } else {
-          randomNumber();
-        }
-      });
-  }, [randomMovie]);
-
+export default function Random(props) {
   return (
     <>
       <div>
-        <button onClick={randomNumber}>Get a random movie</button>
-        <h2>{movieData.title}</h2>
-        <span>{movieData.overview}</span>
+        <button onClick={props.randomNumber}>Get a random movie</button>
+        <h2>{props.movieData.title}</h2>
+        <span>{props.movieData.overview}</span>
 
         <img
           className="moviePoster"
-          src={movieData.poster}
+          src={props.movieData.poster}
           alt="Movie poster"
         />
       </div>
