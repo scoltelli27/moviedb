a3495c709e98c4f6b67a8546af0d4f874dad6752
scoltelli27 <92338065+scoltelli27@users.noreply.github.com>
2022-09-08 16:15:30 -0300

    Random button now working
    Random button now working properly, using state to store the data and render it

---
+++ src/components/Render.js
@@ -0,0 +1,4 @@
+import React, { useState, useEffect } from "react";
+import Searchbar from "./components/Searchbar";
+
+export default function Render(props) {}
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,9 +2,13 @@ import React, { useState, useEffect } from "react";
 
 export default function Searchbar() {
   const [randomMovie, setRandomMovie] = useState();
-
+  const [movieData, setMovieData] = useState({
+    title: "",
+    overview: "",
+    poster: "",
+  });
   function randomNumber() {
-    setRandomMovie(Math.floor(Math.random() * 500));
+    setRandomMovie(Math.floor(Math.random() * 803033));
   }
   useEffect(() => {
     let obj;
@@ -12,17 +16,22 @@ export default function Searchbar() {
       `https://api.themoviedb.org/3/movie/${randomMovie}?api_key=53bdb2a3faf53552f09348f942d242c9`
     )
       .then((res) => res.json())
-      .then((data) => (obj = data))
-      .then(() => console.log(obj.overview));
+      .then((data) =>
+        setMovieData({
+          title: data.title,
+          overview: data.overview,
+          poster: data.poster_path,
+        })
+      );
   }, [randomMovie]);
 
   return (
     <>
       <div>
         <button onClick={randomNumber}>Get a random movie</button>
-      </div>
-      <div>
-        <h2></h2>
+        <h2>{movieData.title}</h2>
+        <span>{movieData.overview}</span>
+        <img url={movieData.poster} />
       </div>
     </>
   );
